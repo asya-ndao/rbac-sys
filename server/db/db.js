d40aeef46270db0e5c09f93f997b7187a48d4782
asya-ndao <sene.asya@gmail.com>
2025-05-18 00:31:11 +0100

    chore: setup MongoDB connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
+
+
 const connectToDatabase = async () => {
 
-    const mongoURI = process.env.MONGODB_UR ?? "mongodb://localhost:27017/rbac-sys";
+    // const mongoURI = process.env.MONGODB_UR ?? "mongodb://localhost:27017/rbac-sys";
 
-    console.log("Mongo uri: ", mongoURI);
+    // console.log("Mongo uri: ", mongoURI);
+
+    const mongoURI = process.env.MONGODB_URI;
+    console.log("Mongo db uri: ", mongoURI);
 
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(mongoURI);
     } catch (error) {
-        console.log(error)   
+        console.log(error);
     }
 }
 
