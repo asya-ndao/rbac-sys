66a5a109d07ecd49214c60d167ec322da527b324
asya-ndao <sene.asya@gmail.com>
2025-05-17 23:59:16 +0100

    feat: Create authentication controller page

---
+++ server/controllers/authController.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken"
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import 'dotenv/config.js'
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Validate the Email if the user exist based on that email
+        const user = await User.findOne({ email })
+        if (!user) {
+            res.status(404).json({ success: false, error: "User Not Found" })
+        }
+        // Verify if the Password is okay..
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            res.status(404).json({ success: false, error: "Wrong Password" })
+        }
+
+        // Generate token by using JWT Token
+        const token = jwt.sign({ _id: user._id, role: user.role },
+            process.env.JWT_KEY, { expiresIn: "10d" }
+        )
+
+        res
+            .status(200)
+            .json({
+                success: true,
+                token,
+                user: { _id: user._id, name: user.name, role: user.role },
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+export { login }
