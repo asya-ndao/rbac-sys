c35969e46d2498b94a02512b2c17bd546ce5ef07
asya-ndao <sene.asya@gmail.com>
2025-05-18 00:13:06 +0100

    feat: Add userSeed script to insert initial admin user

--- server/userSeed.js
+++ server/userSeed.js
@@ -1,20 +1,37 @@
+import bcrypt from "bcryptjs"
+
 import User from "./models/User.js"
-import bcrypt from "bcrypt"
 import connectToDatabase from "./db/db.js"
 
 const userRegister = async () => {
     await connectToDatabase()
     try {
-        const hashPassword = await bcrypt.hash("admin", 10)
-        const newUser = new User({
+        const hashPassword = await bcrypt.hash("admin", 10);
+
+        const user = {
             name: "Admin",
-            email: "admin@gamil.com",
+            email: "admin@gmail.com",
             password: hashPassword,
-            role: "admin"
-        })
-        await newUser.save()
+            role: "admin",
+        }
+
+        const adminExist = await User.findOne({ email: user.email, role: "admin" });
+
+        if (adminExist) {
+            console.log("This admin already exist!!! Good bye.");
+            process.exit(0);
+        }
+
+        const newUser = new User(user);
+        await newUser.save();
+
+        
+        console.log("Admin user has been added successfully !");
     } catch (error) {
-        console.log(error)
+        console.log(error);
+    } finally {
+        console.log("Program is exiting !");
+        process.exit(1);
     }
 }
 
