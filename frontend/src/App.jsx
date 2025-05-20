18f08b187da2150ee3ed0024b5d9303ef3487b58
asya-ndao <sene.asya@gmail.com>
2025-05-20 22:45:09 +0100

    ref: Add employee-dashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
+import { AuthProvider } from "./contexts/authContext";
+import EmployeeDashboard from "./pages/EmployeeDashboard";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
-        <Route path="/Login" element={<Login/>}></Route>
-        <Route path="/admin-dashboard" element={<AdminDashboard/>}></Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+          <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+          <Route path="/employee-dashboard" element={<EmployeeDashboard />}></Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
