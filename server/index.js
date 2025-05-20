81903084c971b2bf52ca0d4546f6ee19b9dc235e
asya-ndao <sene.asya@gmail.com>
2025-05-20 23:17:36 +0100

    feat: Connect to mongodb on server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from "cors";
-import router from './routes/auth.js';
+import router from "./routes/auth.js"
 import connectToDatabase from "./db/db.js"
 
-
+connectToDatabase()
 const app = express()
 app.use(cors())
 app.use(express.json())
