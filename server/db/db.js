263e854c7ac597e11cc849b08eb70642a920443b
asya-ndao <sene.asya@gmail.com>
2025-05-20 22:46:46 +0100

    chores: Remove comments

--- server/db/db.js
+++ server/db/db.js
@@ -4,10 +4,6 @@ import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
 
-    // const mongoURI = process.env.MONGODB_UR ?? "mongodb://localhost:27017/rbac-sys";
-
-    // console.log("Mongo uri: ", mongoURI);
-
     const mongoURI = process.env.MONGODB_URI;
     console.log("Mongo db uri: ", mongoURI);
 
